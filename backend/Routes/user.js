7812474a415c8af6f0dfa44279f4261cde7fe125
Praneerdy <167150186+Praneerdy@users.noreply.github.com>
2024-04-16 15:28:53 +0530

    Delete backend/Routes/user.js

--- backend/Routes/user.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import {getuser,getAlluser} from "../Controllers/userController.js"
-const router=express.Router();
-router.get(('/:id,getuser'))
-router.get(('/,getAlluser'))
-
-
-
-export default router
