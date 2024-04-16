7dadd0f88f33a64be089653f6eeb0b6c3101e436
Praneerdy <167150186+Praneerdy@users.noreply.github.com>
2024-04-16 15:31:12 +0530

    Delete Routes directory

--- Routes/book.js
+++
@@ -1,6 +0,0 @@
-import express from 'express'
-import {book} from '../Controllers/bookController.js'
-const router=express.Router()
-router.post('/bookvenue',book)
-
-export default router;
--- Routes/venue.js
+++
@@ -1,6 +0,0 @@
-import express from 'express'
-import {venue} from '../Controllers/venueController.js'
-const router=express.Router()
-router.post('/bookingform',venue)
-
-export default router;
